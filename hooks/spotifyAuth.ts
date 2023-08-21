a9aba8e6c0a0bda44f61c4d824eae95cf525a05f
Andre Ribeiro <andresmribeiro@gmail.com>
2023-08-21 15:20:03 +0100

    feat: expo auth with spotify

---
+++ hooks/spotifyAuth.ts
@@ -0,0 +1,31 @@
+import * as WebBrowser from 'expo-web-browser';
+import {makeRedirectUri, useAuthRequest} from 'expo-auth-session';
+
+// Ensure that you initialize the WebBrowser:
+WebBrowser.maybeCompleteAuthSession();
+
+const discovery = {
+    authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+    tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
+
+export default function useSpotifyAuth() {
+    const redirectUri = makeRedirectUri();
+    console.log('redirectUri', redirectUri)
+    console.log('process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID', process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID)
+    const [request, response, promptAsync] = useAuthRequest(
+        {
+            clientId: process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID!!,
+            scopes: ['user-read-private', 'playlist-modify-public'], // Add more as needed
+            redirectUri,
+            responseType: 'token',
+        },
+        discovery
+    );
+
+    return {
+        request,
+        response,
+        promptAsync,
+    };
+}
