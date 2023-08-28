015287cd2e774cbb34c9ed0777677521c865727d
Andre Ribeiro <andresmribeiro@gmail.com>
2023-08-28 17:30:55 +0100

    feat: fetch song info from backend

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,78 @@
 import { StyleSheet } from 'react-native';
+import { Image, Text, View } from 'tamagui';
+import { useEffect, useState } from 'react';
+import Swiper from 'react-native-swiper';
+import { Song } from '@/types/songs';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from 'tamagui';
+const swiperColors = ['#4ea821', '#97CAE5', '#e8e423', '#a8214e', '#e5ca97', '#23e8e4'];
+
+function formatDate(date: Date) {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
+const url = `${process.env.EXPO_PUBLIC_BACKEND_URL!}/songs?day=${formatDate(new Date())}`;
 
 export default function TabTwoScreen() {
+    const [songs, setSongs] = useState<Song[]>([]);
+
+    useEffect(() => {
+        console.log(url);
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => setSongs(data))
+            .catch((error) => console.error(error));
+    }, []);
+
+    if (songs.length === 0) {
+        return (
+            <View>
+                <Text>Loading...</Text>
+            </View>
+        );
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab Two</Text>
-            <View style={styles.separator} />
-            <EditScreenInfo path="app/(tabs)/two.tsx" />
-        </View>
+        <Swiper style={styles.wrapper} showsButtons loop={false}>
+            {songs.map((song, index) => {
+                return (
+                    <View key={index} style={styles.slide1} backgroundColor={swiperColors[index % swiperColors.length]}>
+                        {song.album_cover && <Image source={{ uri: song.album_cover, width: 100, height: 100 }} />}
+                        <Text style={styles.text}>{song.genre}</Text>
+                        <Text style={styles.text}>{song.artist}</Text>
+                        <Text style={styles.text}>{song.title}</Text>
+                    </View>
+                );
+            })}
+        </Swiper>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    wrapper: {},
+    slide1: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
     },
-    title: {
-        fontSize: 20,
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#97CAE5',
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e8e423',
+    },
+    text: {
+        color: '#000',
+        fontSize: 30,
         fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
     },
 });
---
+++ types/songs.ts
@@ -0,0 +1,11 @@
+export type Song = {
+    artist: string;
+    created_at: string;
+    description: string;
+    genre: string;
+    id: number;
+    link: string;
+    overview: string;
+    title: string;
+    album_cover: string;
+};
